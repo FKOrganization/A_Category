09e9bd80cb2a927739bff2173911de6eee04866b
test－mac <team@testmacdeMac-mini.local>
2018-08-10 15:11:00 +0800

    修改数据

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by test－mac on 2018/8/10.
+//  Copyright © 2018年 FK. All rights reserved.
+//
+
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+
+@interface CTMediator (A)
+
+- (UIViewController *)A_ViewController;
+
+@end
